66bde75e1d3ca0c826686b712857f9cc02ea5039
Andre Odendaal <aodendaal@gmail.com>
2025-08-10 21:33:43 +0200

    Test old instructions on new robot

--- robot-rover-test/MoveBackRobot.cs
+++ robot-rover-test/MoveBackRobot.cs
@@ -13,5 +13,16 @@ namespace RobotRover.Test
             Assert.AreEqual(5, robot.X);
             Assert.AreEqual(Direction.North, robot.Facing);
         }
+
+        [TestMethod]
+        public void CanExecuteNewAndCoreInstructions()
+        {
+            var robot = new MoveBackRobot(new Planet(10, 10), 5, 5, Direction.North);
+            robot.ExecuteInstructions("BRBR");
+
+            Assert.AreEqual(4, robot.Y);
+            Assert.AreEqual(4, robot.X);
+            Assert.AreEqual(Direction.South, robot.Facing);
+        }
     }
 }
