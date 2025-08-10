71ca40c2ea7f7fd250804b7e30b17c3c436c1f3b
Andre Odendaal <aodendaal@gmail.com>
2025-08-10 18:32:50 +0200

    Add robot turning instructions

--- robot-rover-test/DefaultInstructions.cs
+++ robot-rover-test/DefaultInstructions.cs
@@ -18,5 +18,37 @@ namespace RobotRover.Test
 
             Assert.AreEqual(3, robot2.X);
         }
+
+        [TestMethod]
+        public void CanTurnLeft()
+        {
+            var robot = new Robot(new Planet(10, 10), 5, 5, Direction.North);
+
+            DefaultInstructions.TurnLeft(robot);
+
+            Assert.AreEqual(Direction.West, robot.Facing);
+
+            var robot2 = new Robot(new Planet(10, 10), 5, 5, Direction.East);
+
+            DefaultInstructions.TurnLeft(robot2);
+
+            Assert.AreEqual(Direction.North, robot2.Facing);
+        }
+
+        [TestMethod]
+        public void CanTurnRight()
+        {
+            var robot = new Robot(new Planet(10, 10), 5, 5, Direction.North);
+
+            DefaultInstructions.TurnRight(robot);
+
+            Assert.AreEqual(Direction.East, robot.Facing);
+
+            var robot2 = new Robot(new Planet(10, 10), 5, 5, Direction.West);
+
+            DefaultInstructions.TurnRight(robot2);
+
+            Assert.AreEqual(Direction.North, robot2.Facing);
+        }   
     }
 }
--- robot-rover/DefaultInstructions.cs
+++ robot-rover/DefaultInstructions.cs
@@ -20,5 +20,43 @@ namespace RobotRover
                     break;
             }
         }
+
+        public static void TurnLeft(IRobot robot)
+        {
+            switch (robot.Facing)
+            {
+                case Direction.North:
+                    robot.Facing = Direction.West;
+                    break;
+                case Direction.West:
+                    robot.Facing = Direction.South;
+                    break;
+                case Direction.South:
+                    robot.Facing = Direction.East;
+                    break;
+                case Direction.East:
+                    robot.Facing = Direction.North;
+                    break;
+            }
+        }
+
+        public static void TurnRight(IRobot robot)
+        {
+            switch (robot.Facing)
+            {
+                case Direction.North:
+                    robot.Facing = Direction.East;
+                    break;
+                case Direction.East:
+                    robot.Facing = Direction.South;
+                    break;
+                case Direction.South:
+                    robot.Facing = Direction.West;
+                    break;
+                case Direction.West:
+                    robot.Facing = Direction.North;
+                    break;
+            }
+        }   
     }
 }
